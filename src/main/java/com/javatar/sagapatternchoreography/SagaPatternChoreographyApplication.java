70b8cdb47f2a6ee955df746f9daf402b6a1232b6
e.sadeghi <e.sadeghi@fwutech.com>
2023-02-24 06:14:13 -0500

    init project

---
+++ src/main/java/com/javatar/sagapatternchoreography/SagaPatternChoreographyApplication.java
@@ -0,0 +1,13 @@
+package com.javatar.sagapatternchoreography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaPatternChoreographyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagaPatternChoreographyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javatar/sagapatternchoreography/SagaPatternChoreographyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatar.sagapatternchoreography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaPatternChoreographyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
